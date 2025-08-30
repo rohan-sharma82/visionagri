aeb8d6d2caeec00c4695e58f3baba4238c332f22
Rohan Sharma <rohansharmaa185@gmail.com>
2025-08-30 06:09:26 +0000

    reduce length of anmated light from the right side a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           <User className="h-24 w-24 md:h-32 md:w-32 text-muted-foreground" />
         </div>
 
-        <div className="absolute top-[30%] left-[calc(10%_+_8rem)] lg:left-[calc(25%_+_9rem)] -translate-y-1/2 z-0 w-[calc(80%_-_16rem)] lg:w-[calc(50%_-_18rem)]">
+        <div className="absolute top-[30%] left-[calc(10%_+_8rem)] lg:left-[calc(25%_+_9rem)] -translate-y-1/2 z-0 w-[calc(80%_-_18rem)] lg:w-[calc(50%_-_18rem)]">
           <svg
             width="100%"
             height="4"
