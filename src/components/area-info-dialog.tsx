7aa0c6dfe4303a7c52d08c2c0a6f284f1f6bea33
Rohan Sharma <rohansharmaa185@gmail.com>
2025-09-03 16:04:26 +0000

    html - <button>   <div class="default-btn">     <svg class="css-i6dzq1"

--- src/components/area-info-dialog.tsx
+++ src/components/area-info-dialog.tsx
@@ -22,14 +22,49 @@ import { Separator } from './ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { useTranslation } from '@/hooks/use-translation';
-import { Button } from './ui/button';
 
 export default function AreaInfoDialog() {
   const { t } = useTranslation();
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="button type1 mb-8">{t('cropYield.buttons.areaInfo')}</Button>
+        <button className="know-your-area-button mb-8">
+          <div className="default-btn">
+            <svg
+              className="css-i6dzq1"
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              fill="none"
+              strokeWidth="2"
+              stroke="#FFF"
+              height="20"
+              width="20"
+              viewBox="0 0 24 24"
+            >
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle r="3" cy="12" cx="12"></circle>
+            </svg>
+            <span>Quick Info</span>
+          </div>
+          <div className="hover-btn">
+            <svg
+              className="css-i6dzq1"
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              fill="none"
+              strokeWidth="2"
+              stroke="#ffd300"
+              height="20"
+              width="20"
+              viewBox="0 0 24 24"
+            >
+              <circle r="1" cy="21" cx="9"></circle>
+              <circle r="1" cy="21" cx="20"></circle>
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg>
+            <span>{t('cropYield.buttons.areaInfo')}</span>
+          </div>
+        </button>
       </DialogTrigger>
       <DialogContent className="max-w-4xl h-[80vh]">
         <DialogHeader>
