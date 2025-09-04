48e66d8947129eb7ac7c4e4ec2c80d386e5e4cf5
Rohan Sharma <rohansharmaa185@gmail.com>
2025-09-04 06:18:36 +0000

    html - <div class="card">     <div class="border"></div>     <div class=

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -196,20 +196,28 @@ export default function Home() {
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.3 }}
               >
-                <div className="card5">
-                  <div className="card5-content">
-                    <div className="card-date">{t(news.date)}</div>
-                    <div className='flex flex-col items-center justify-center h-full'>
-                      <span className="card-title">{news.title}</span>
-                      <p className="card-description">{news.snippet}</p>
-                    </div>
-                    <DialogTrigger asChild>
-                      <button onClick={() => setSelectedNews(news)} className="card-link">
-                        {t('home.newsFeed.readMore')}
-                      </button>
-                    </DialogTrigger>
+                <DialogTrigger asChild>
+                  <div className="card" onClick={() => setSelectedNews(news)}>
+                      <div className="border"></div>
+                      <div className="content">
+                          <div className="logo">
+                              <div className="logo1">
+                                  <svg viewBox="0 0 29.667 31.69" xmlns="http://www.w3.org/2000/svg" id="logo-main">
+                                      <path transform="translate(0 0)" d="M12.827,1.628A1.561,1.561,0,0,1,14.31,0h2.964a1.561,1.561,0,0,1,1.483,1.628v11.9a9.252,9.252,0,0,1-2.432,6.852q-2.432,2.409-6.963,2.409T2.4,20.452Q0,18.094,0,13.669V1.628A1.561,1.561,0,0,1,1.483,0h2.98A1.561,1.561,0,0,1,5.947,1.628V13.191a5.635,5.635,0,0,0,.85,3.451,3.153,3.153,0,0,0,2.632,1.094,3.032,3.032,0,0,0,2.582-1.076,5.836,5.836,0,0,0,.816-3.486Z" data-name="Path 6" id="Path_6"></path>
+                                      <path transform="translate(-45.91 0)" d="M75.207,20.857a1.561,1.561,0,0,1-1.483,1.628h-2.98a1.561,1.561,0,0,1-1.483-1.628V1.628A1.561,1.561,0,0,1,70.743,0h2.98a1.561,1.561,0,0,1,1.483,1.628Z" data-name="Path 7" id="Path_7"></path>
+                                      <path transform="translate(0 -51.963)" d="M0,80.018A1.561,1.561,0,0,1,1.483,78.39h26.7a1.561,1.561,0,0,1,1.483,1.628v2.006a1.561,1.561,0,0,1-1.483,1.628H1.483A1.561,1.561,0,0,1,0,82.025Z" data-name="Path 8" id="Path_8"></path>
+                                  </svg>
+                              </div>
+                              <div className="logo2">
+                                <p className="text-white text-xs text-center font-bold px-2 line-clamp-4">{news.title}</p>
+                              </div>
+                              <span className="trail"></span>
+                          </div>
+                          <span className="logo-bottom-text">{t(news.source)}</span>
+                      </div>
+                      <span className="bottom-text">{t(news.date)}</span>
                   </div>
-                </div>
+                </DialogTrigger>
               </motion.div>
             ))}
           </div>
