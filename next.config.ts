4ec8f38f6c203cfec09815315e550cc09e5ad2a7
Rohan Sharma <rohansharmaa185@gmail.com>
2025-09-11 16:26:14 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -134,6 +134,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.godigit.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
